a1729f3dc888c50ce583750bcf396aeb877ed3fd
danyd3 <89928594+danyd3@users.noreply.github.com>
2021-10-12 12:39:27 -0500

    Update rinecuaciones.m

--- rinecuaciones.m
+++ rinecuaciones.m
@@ -1,7 +1,7 @@
 % Octave Script 
 % Title              :Axiomas algebraicos
 % Description        :Script para resolver inecuaciones
-% Author             :Daniel Alberto De Jesus (danyd3), Cesar Manrique De Jesús (Mnrq_96)
+% Author             :Daniel Alberto De Jesus (danyd3), Cesar Manrique De JesÃºs (Mnrq_96)
 % Date               :20211007
 % Version            :1
 % Usage              :octave> rinecuaciones
@@ -9,26 +9,49 @@
 %                    :https://octaveintro.readthedocs.io/en/latest/index.html
 
 clear
-p_paso1 = 'Representar la ecuación es su forma general (a x 2 + b x + c)';
-p_paso2 = 'Factorizar la ecuación desde la izquierda';
+p_paso1 = 'Representar la ecuaciÃ³n es su forma general (a x 2 + b x + c)';
+p_paso2 = 'Factorizar la ecuaciÃ³n desde la izquierda';
 p_paso3 = 'Hallar los intervalos de prueba';
 p_paso4 = 'Representar los intervalos graficamente en una recta numerica'; 
 
 %INECUACIONES DE PRIMER GRADO
+syms x
 disp('INECUACIONES DE PRIMER GRADO')
 pkg load symbolic
-syms x
-solve(x-6<0,x)
+x-6<0
+solve(x-6<0)
 disp('Define la variable simbolico x')
 
 %INECUACIONES DE SEGUNDO GRADO
 disp('INECUACIONES DE SEGUNDO GRADO')
 syms x
+x^3+19<77
 solve(x^3+19<77)
 disp('Define la variable simbolico x')
 
 %INECUACIONES DE TERCER GRADO
 disp('INECUACIONES DE TERCER GRADO')
 syms x
-solve(9x-12<24)
+abs(x) < 5
+solver(abdominales(x) < 5
 disp('Define la variable simbolico x')
+
+espera
+
+diagrama([-20 5],[0,0],'k+-',"ancho de linea",3,"tamaÃ±o de marcador",12
+parcela([0 0],[-20 30], 'k-', "ancho de linea",3)
+
+x = -20:0.01:11;
+y = x-6<0;
+grafica(x,y,"ancho de linea",2)
+
+x = -100:1:100;
+f = 'x^3+19<77';
+fplot(f,"ancho de linea",2);
+
+x = -100:1:100;
+y2 = abdominales(x)<5;
+grafica(x,y2,"ancho de linea",2)
+
+espera
+
