1d93b28a90971c4d56ff401788f8903606ab6b8b
Jose Manuel Arce <91504311+JoseManuelArceSal@users.noreply.github.com>
2021-10-14 13:17:38 -0500

    informacion
    datos adicionales

--- damasa.m
+++ damasa.m
@@ -5,21 +5,27 @@
 % Author                 :Sabrina Andrea Hernandez (Gitsabrinax), Daniel Alberto De Jesús Dominguez (danyd3), Jose Manuel Arce Salvador (JoseManuelArceSal)
 % Date                   :20211013
 % Version                :1
-% Usage                  : 
+% Usage                  :
 % Notes                  :Requiere Octave
-
+%                        :https://octaveintro.readthedocs.io/en/latest/index.html
 % limpiar variables
 clear
+
 % Ejemplo mínimo de material para envase cilindrico
+
 % Rango de 0.0.1..59.4 i:0.0
+
 r=0:0.1:59.4;
 r2=0:0.1:60.60;
 % Valor de la funcion
+
 x=3*r
 y=2*r2
 % función plotear
+
 a=x+y
 %función para determinar valor mínino
+
 rest=fminbnd(ar,0,1);
 %Dibujar x.y
 plot(x, y)
